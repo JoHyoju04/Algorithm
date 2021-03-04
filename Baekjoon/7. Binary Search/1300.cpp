9fdd543a82bb462e0ede99017a05a90e9a01e8de
JoHyoju04 <whgywn369@gmail.com>
2021-03-04 11:44:48 +0900

    04.MAR.2021

---
+++ Baekjoon/7. Binary Search/1300.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+long long n, k;
+
+long long func(long long mid) // mid의 인덱스를 구하는 함수
+{
+	long long cnt = 0;
+	for (long long i = 1; i <= n; i++)
+	{ // 현재 원소(mid)보다 작거나 같은 수의 개수를 count
+		cnt += min(n, mid / i);
+	}
+	return cnt; // mid의 인덱스
+}
+
+int main(void)
+{
+	cin >> n >> k;
+
+	long long left = 1;
+	long long right = n * n;
+	long long ans = 1;
+	while (left <= right)
+	{
+		long long mid = (left + right) / 2; // 현재 원소
+
+		if (func(mid) >= k) // 현재 원소의 인덱스가 더 크다면
+		{
+			ans = mid;
+			right = mid - 1; // 현재 원소를 감소시켜 확인
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	cout << ans << '\n';
+}
