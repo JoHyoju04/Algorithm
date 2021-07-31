c7393d6c346c75b7f03b22daf2941589e87c180e
JoHyoju04 <whgywn369@gmail.com>
2021-07-31 23:00:12 +0900

    31.JUL.2021

---
+++ Baekjoon/3. BF(Brute-Force)/16508.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+struct book {
+    int cost;
+    char title[51];
+};
+char t[11];
+int n;
+book arr[16];
+int cnt[26];
+int select_cnt[26];
+int ans = 987654321;
+ 
+bool check() {
+    for (int i = 0; i < 26; i++)
+        if (cnt[i] > select_cnt[i])
+            return false;
+    return true;
+}
+ 
+void dfs(int idx, int total) {
+    if (idx == n) {
+        if (check())
+            ans = min(ans, total);
+        return;
+    }
+    for (int i = 0; arr[idx].title[i] != '\0'; i++)
+        select_cnt[arr[idx].title[i] - 'A']++;
+    dfs(idx + 1, total + arr[idx].cost);
+    for (int i = 0; arr[idx].title[i] != '\0'; i++)
+        select_cnt[arr[idx].title[i] - 'A']--;
+    dfs(idx + 1, total);
+}
+ 
+int main() {
+    cin >> t;
+    for (int i = 0; t[i] != '\0'; i++)
+        cnt[t[i] - 'A']++;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].cost >> arr[i].title;
+    }
+    dfs(0, 0);
+    cout << (ans == 987654321 ? -1 : ans) << '\n';
+    return 0;
+}
