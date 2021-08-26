207c9e05fcc4636b827122e0a20c8e53092ea8f5
JoHyoju04 <whgywn369@gmail.com>
2021-08-26 23:35:39 +0900

    26.AUG.2021

--- Programmers/level3/셔틀버스.cpp
+++ Programmers/level3/셔틀버스.cpp
@@ -2,61 +2,45 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
- 
-string solution(int n, int t, int m, vector<string> timetable) 
-{
-    vector<int> Crew;
+
+string solution(int n, int t, int m, vector<string> timetable) {
+    vector<int> crew;
     for (int i = 0; i < timetable.size(); i++)
     {
-        string S_Hour = "";
-        S_Hour = S_Hour + timetable[i][0];
-        S_Hour = S_Hour + timetable[i][1];
-        int Hour = stoi(S_Hour);
-        
-        string S_Minute = "";
-        S_Minute = S_Minute + timetable[i][3];
-        S_Minute = S_Minute + timetable[i][4];
-        int Minute = stoi(S_Minute);
-        
-        int Time = Hour * 60 + Minute;
-        Crew.push_back(Time);
+        string ts=timetable[i];
+        int hour=stoi(ts.substr(0,2));
+        int mini=stoi(ts.substr(3,2));
+        crew.push_back(hour*60+mini);
+
     }
-    sort(Crew.begin(), Crew.end());
+    sort(crew.begin(), crew.end());
  
-    int Shuttle_Time = 540;
-    int Crew_Idx = 0;
-    int Answer_Time;
-    for (int i = 1; i <= n; i++, Shuttle_Time = Shuttle_Time + t)
+    int shuttleTime = 540;
+    int crewIdx = 0;
+    int answerTime;
+    for (int i = 1; i <= n; i++, shuttleTime = shuttleTime + t)
     {
-        int Cnt = 0;
-        for (int j = Crew_Idx; j < Crew.size(); j++)
+        int cnt = 0;
+        for (int j = crewIdx; j < crew.size(); j++)
         {
-            if (Crew[j] <= Shuttle_Time)
+            if (crew[j] <= shuttleTime)
             {
-                Crew_Idx++;
-                Cnt++;
-                if (Cnt == m) break;
+                crewIdx++;
+                cnt++;
+                if (cnt == m) break;
             }
         }
  
         if (i == n)
         {
-            if (Cnt == m) Answer_Time = Crew[Crew_Idx - 1] - 1;
-            else Answer_Time = Shuttle_Time;
+            if (cnt == m) answerTime = crew[crewIdx - 1] - 1;
+            else answerTime = shuttleTime;
         }
     }
-    
-    string answer = "";
-    int Hour = Answer_Time / 60;
-    int Minute = Answer_Time % 60;
-    char A = Hour / 10 + '0';
-    char B = Hour % 10 + '0';
-    char C = Minute / 10 + '0';
-    char D = Minute % 10 + '0';
-    answer = answer + A;
-    answer = answer + B;
-    answer = answer + ':';
-    answer = answer + C;
-    answer = answer + D;
+    string hour=to_string(answerTime/60);
+    string mini=to_string(answerTime%60);
+    if(hour.size()!=2)  hour.insert(0,"0");
+    if(mini.size()!=2)  mini.insert(0,"0");
+    string answer=hour+":"+mini;
     return answer;
 }
