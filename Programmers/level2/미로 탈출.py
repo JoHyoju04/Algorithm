c16981819b9b96574883b38266e33ef0acc899ae
JoHyoju04 <whgywn369@gmail.com>
2024-06-18 11:43:26 +0900

    18.JUN.2024

--- Programmers/level2/미로 탈출.py
+++ Programmers/level2/미로 탈출.py
@@ -1,4 +1,49 @@
 from collections import deque
+dirs=[[0,1],[0,-1],[1,0],[-1,0]]
+def bfs(startLoc,endLoc,maps):
+    visited=[[0 for _ in range(C)] for _ in range(R)]
+    que=deque()
+    visited[startLoc[0]][startLoc[1]]=1
+    que.append(startLoc)
+    
+    while que:
+        curLoc=que.popleft()
+        if curLoc[0]==endLoc[0] and curLoc[1]==endLoc[1]:
+            return visited[endLoc[0]][endLoc[1]]-1
+        for i,j in dirs:
+            nr=curLoc[0]+i
+            nc=curLoc[1]+j
+            if nr<0 or nr>=R or nc<0 or nc>=C or visited[nr][nc]>0:
+                continue
+            if maps[nr][nc]!='X':
+                visited[nr][nc]=visited[curLoc[0]][curLoc[1]]+1
+                que.append([nr,nc])
+    return -1
+                
+def solution(maps):
+    global R,C
+    answer=0
+    R=len(maps)
+    C=len(maps[0])
+    startLoc,leverLoc,exitLoc=[0,0],[0,0],[0,0]
+    for i in range(R):
+        for j in range(C):
+            if maps[i][j]=='S':
+                startLoc=[i,j]
+            elif maps[i][j]=='E':
+                exitLoc=[i,j]
+            elif maps[i][j]=='L':
+                leverLoc=[i,j]
+    startToLeverTime=bfs(startLoc,leverLoc,maps)
+    if startToLeverTime==-1:
+        return -1
+    leverToExitTime=bfs(leverLoc,exitLoc,maps)
+    if leverToExitTime==-1:
+        return -1
+    return startToLeverTime+leverToExitTime
+                
+'''
+from collections import deque
 def solution(maps):
     answer = -1
     R,C=len(maps),len(maps[0])
@@ -31,3 +76,4 @@ def solution(maps):
                     visited[1][nr][nc]=visited[lever][cr][cc]+1
     
     return answer
+'''
--- Programmers/level2/이모티콘 할인행사.py
+++ Programmers/level2/이모티콘 할인행사.py
@@ -1,3 +1,30 @@
+candidates=[]
+def dfs(depth,choices,users,emoticons):
+    global candidates
+    if depth==len(emoticons):
+        signUsers,salesAmount=0,0
+        for user in users:
+            purchaseAmount=0
+            for emoticonsIdx in range(len(emoticons)):
+                if user[0]<=choices[emoticonsIdx]:
+                    purchaseAmount+=int(emoticons[emoticonsIdx]*((100-choices[emoticonsIdx])*0.01))
+            if user[1]<=purchaseAmount:
+                signUsers+=1
+            else:
+                salesAmount+=purchaseAmount
+        candidates.append([signUsers,salesAmount])
+        return
+                    
+    for rate in [10,20,30,40]:
+        choices.append(rate)
+        dfs(depth+1,choices,users,emoticons)
+        choices.pop()
+def solution(users,emoticons):
+    dfs(0,[],users,emoticons)
+    candidates.sort(reverse=True)
+    return candidates[0]
+
+'''
 discounts=[]
 percents=[10,20,30,40]
 candidate = []
@@ -32,3 +59,4 @@ def solution(users, emoticons):
     candidate = sorted(candidate,key=lambda x:(-x[0],-x[1]))
     answer=candidate[0]
     return answer
+'''
